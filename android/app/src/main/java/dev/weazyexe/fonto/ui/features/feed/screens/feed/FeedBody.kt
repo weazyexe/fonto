61543378b3e09e59133fccf9b4e41f2b0db77b4a
Timur Zadvornov <weazy.exe@gmail.com>
2023-06-01 21:12:01 +0400

    fixed newsline sorting

--- android/app/src/main/java/dev/weazyexe/fonto/ui/features/feed/screens/feed/FeedBody.kt
+++ android/app/src/main/java/dev/weazyexe/fonto/ui/features/feed/screens/feed/FeedBody.kt
@@ -1,17 +1,13 @@
 package dev.weazyexe.fonto.ui.features.feed.screens.feed
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.tooling.preview.Preview
 import dev.weazyexe.fonto.core.ui.ScrollState
@@ -154,24 +150,6 @@ private fun FeedBodyPreview() = ThemedPreview {
         fetchNextBatch = {},
         onSearchBarActiveChange = {}
     )
-}
-
-@Composable
-private fun LazyListState.isScrollingUp(): State<Boolean> {
-    var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
-    var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
-    return remember(this) {
-        derivedStateOf {
-            if (previousIndex != firstVisibleItemIndex) {
-                previousIndex > firstVisibleItemIndex
-            } else {
-                previousScrollOffset >= firstVisibleItemScrollOffset
-            }.also {
-                previousIndex = firstVisibleItemIndex
-                previousScrollOffset = firstVisibleItemScrollOffset
-            }
-        }
-    }
 }
 
 @Preview
